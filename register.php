8c87dd174161a8920bb4998b262b9465ae260ff2
Aryan <Aryan.gharshi@gmail.com>
2019-03-07 15:19:28 +0100

    save

--- register.php
+++ register.php
@@ -9,11 +9,11 @@
 <body onload="init()">
 <?php include "inc/nav.php"?>
 <section id="register">
-    <form action="">
+    <form method="POST" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" enctype="multipart/form-data">
         <legend>Register</legend>
 
-        <label for="fname">Serial Number<span>&#8226;</span></label>
-        <input type="text" id="fname" required>
+        <label for="serial">Insert your serial number<span>&#8226;</span></label>
+        <input type="text" id="serial" name="serial" required>
 
         <!-- <label for="lname">Last name <span>&#8226;</span></label>
         <input type="text" id="lname" required>
@@ -27,9 +27,13 @@
         <label for="username">Username <span>&#8226;</span></label>
         <input type="text" id="username" required> -->
 
-        <label for="password">Password <span>&#8226;</span></label>
-        <input type="password" pattern=".{6,}" title="Six or more characters" id="password" required>
+        <label for="password1">Choose a password <span>&#8226;</span></label>
+        <input type="password" name="password1" pattern=".{6,}" title="Six or more characters" id="password1" required>
 
+        <label for="password2">Repeat password <span>&#8226;</span></label>
+        <input type="password" name="password2" pattern=".{6,}" title="Six or more characters" id="password2" required>
+
+        <!--
         <label for="departurepoint">Preferred departure point <span>&#8226;</span> </label>
         <select id="departurepoint" name="cars">
             <option value="moscow">Russia - Moscow</option>
@@ -37,10 +41,55 @@
             <option value="washington">USA - Washington DC</option>
             <option value="dubai">UAE - Dubai</option>
         </select>
+        -->
 
         <input type="submit" id="registerbutton" value="Register">
 
     </form>
+
+    <?php
+    include "inc/connection.php";
+
+
+    if ( isset($_POST["serial"]) && isset($_POST["password1"]) && isset($_POST["password2"]) ){
+        $serial = check($_POST["serial"]);
+        $pass1 = check($_POST["password1"]);
+        $pass2 = check($_POST["password2"]);
+        $pass = null;
+
+        # makes sure the two emails match
+        if ($pass1 != $pass2){
+            echo "<script type='text/javascript'> alert('the passwords are not identical');</script>";
+            die();
+
+        }else{
+            $pass1 = password_hash($pass1, PASSWORD_DEFAULT);
+        }
+
+        $query = "INSERT INTO iter_users 
+                  VALUES ('$serial','$pass1', NULL, NULL)";
+
+        $result = $conn->query($query);
+        if ($result === TRUE) {
+            echo '<script language="javascript">';
+            echo 'alert("Registration complete.")';
+            echo '</script>';
+            header('LOCATION:login.php');
+        } else {
+            $err = $conn->error;
+            echo '<script language="javascript">';
+            echo 'alert("Sorry: '.$err.'")';
+            echo '</script>';
+        }
+    }
+    $conn->close();
+
+
+
+
+
+
+    ?>
 </section>
 <?php include "inc/footer.php"?>
 

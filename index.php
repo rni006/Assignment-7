9f3be3b825adc179cced4ef1b097efe80f414a17
Wiwalla <r.nikolajeva@gmail.com>
2019-04-14 21:27:29 +0200

    added picture gallery
    not super "phone friendly" yet

--- index.php
+++ index.php
@@ -34,7 +34,76 @@
             </p>
         </div>
     </section>
-    <section id="experience"></section>
+
+
+
+    <section id="experience">
+        <h1 id="experinace-title">Experiance the world beyond</h1>
+    <div class="slideshow-container">
+<div class="mySlides fade">
+  <div class="numbertext">1 / 3</div>
+  <img src="images/modernmars1.jpg" style="width:100%">
+  <div class="text">Experiance the modern mars</div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">2 / 3</div>
+  <img src="images/download1.jpg" style="width:100%">
+  <div class="text">Fly through the sky</div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">3 / 3</div>
+  <img src="images/ancient1.jpg" style="width:100%">
+  <div class="text">Visit the ancient ruins of Kalebo III</div>
+</div>
+
+<a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+<a class="next" onclick="plusSlides(1)">&#10095;</a>
+
+</div>
+<br>
+
+<div style="text-align:center">
+  <span class="dot" onclick="currentSlide(1)"></span> 
+  <span class="dot" onclick="currentSlide(2)"></span> 
+  <span class="dot" onclick="currentSlide(3)"></span> 
+</div>
+
+<!-- SCRIPT -->
+
+<script>
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+</script>
+
+    </section>
+
+
     <section id="events">
         <div id="content">
             <h2>Events</h2>
